f91afef999fcb7bf4ed6d26256e3d23d4e1e642d
KyleM73 <kyle.morgenstein@gmail.com>
2022-11-23 02:08:27 -0500

    test envc

--- mrsearch_IG_RL/envs/base_env.py
+++ mrsearch_IG_RL/envs/base_env.py
@@ -9,17 +9,18 @@ import pybullet as p
 import pybullet_utils.bullet_client as bc
 import pybullet_data
 
+import EnvCreator
+
 PATH_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-class base_env(env):
+class base_env(Env):
     def __init__(self,training=True,cfg=None):
-        if ifinstance(cfg,str):
+        if isinstance(cfg,str):
             assert os.path.exists(cfg), "configuration file specified does not exist"
-            cfg = yaml.load(cfg)
+            with open(cfg, 'r') as stream:
+                self.cfg = yaml.safe_load(stream)#,Loader=yaml.FullLoader)
         else:
             raise AssertionError("no configuration file specified")
-
-        self.cfg = cfg
 
         if training:
             self.client = bc.BulletClient(connection_mode=p.DIRECT)
@@ -30,8 +31,8 @@ class base_env(env):
         self.training = training
         
         ## simulation params
-        self.horizon = self.cfg["horizon"]
-        self.dt = self.cfg["dt"]
+        self.horizon = self.cfg["simulation"]["horizon"]
+        self.dt = self.cfg["simulation"]["dt"]
 
         ## environment params
         self.resolution = self.cfg["environment"]["resolution"]
@@ -40,14 +41,14 @@ class base_env(env):
         self.fname = self.cfg["environment"]["filename"]
         print("generating urdf...")
         self.env_c = EnvCreator.envCreator(PATH_DIR+self.fname)
-        self.env_urdf = env_c.get_urdf_fast()
+        self.env_urdf = self.env_c.get_urdf_fast()
         print("done.")
 
         self.obs_space = spaces.Box(low=-1,high=1,shape=(self.h,self.w),dtype=np.float32)
         self.action_space = spaces.Box(low=0,high=1,shape=(2,),dtype=np.float32)
 
     def reset(self):
-        
+
         ## initiate simulation
         self.client.resetSimulation()
         self.client.setTimeStep(self.dt)
@@ -66,7 +67,7 @@ class base_env(env):
         self._get_rew()
 
     def step(self):
-        pass
+        self.client.stepSimulation()
 
     def render(self):
         pass
@@ -76,6 +77,15 @@ class base_env(env):
 
     def _get_rew(self):
         pass
+
+if __name__ == "__main__":
+
+    env = base_env(False,PATH_DIR+"/cfg/base_env.yaml")
+    env.reset()
+
+    for _ in range(10000):
+        env.step()
+
 
         
 
