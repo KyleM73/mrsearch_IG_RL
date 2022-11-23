f40a96212d3852454d6f2999b99cde4d8e4d25f6
KyleM73 <kyle.morgenstein@gmail.com>
2022-11-22 22:52:53 -0500

    reset

--- mrsearch_IG_RL/envs/base_env.py
+++ mrsearch_IG_RL/envs/base_env.py
@@ -9,13 +9,15 @@ import pybullet as p
 import pybullet_utils.bullet_client as bc
 import pybullet_data
 
+PATH_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 class base_env(env):
     def __init__(self,training=True,cfg=None):
         if ifinstance(cfg,str):
-            assert os.path.exists(cfg), "Configuration file specified does not exist"
+            assert os.path.exists(cfg), "configuration file specified does not exist"
             cfg = yaml.load(cfg)
         else:
-            raise AssertionError("No configuration file specified")
+            raise AssertionError("no configuration file specified")
 
         self.cfg = cfg
 
@@ -35,12 +37,33 @@ class base_env(env):
         self.resolution = self.cfg["environment"]["resolution"]
         self.h = int(self.cfg["environment"]["height"] * self.resolution)
         self.w = int(self.cfg["environment"]["width"] * self.resolution)
+        self.fname = self.cfg["environment"]["filename"]
+        print("generating urdf...")
+        self.env_c = EnvCreator.envCreator(PATH_DIR+self.fname)
+        self.env_urdf = env_c.get_urdf_fast()
+        print("done.")
 
         self.obs_space = spaces.Box(low=-1,high=1,shape=(self.h,self.w),dtype=np.float32)
         self.action_space = spaces.Box(low=0,high=1,shape=(2,),dtype=np.float32)
 
     def reset(self):
-        pass
+        
+        ## initiate simulation
+        self.client.resetSimulation()
+        self.client.setTimeStep(self.dt)
+        self.client.setGravity(0,0,-9.8)
+
+        ## setup ground
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        self.ground = p.loadURDF("plane.urdf")
+        p.changeDynamics(self.ground, -1, lateralFriction=0.0) 
+
+        ## setup walls
+        self.walls = p.loadURDF(self.env_urdf,useFixedBase=True)
+        p.changeDynamics(self.walls, -1, lateralFriction=1.0)
+
+        self._get_obs()
+        self._get_rew()
 
     def step(self):
         pass
@@ -48,17 +71,15 @@ class base_env(env):
     def render(self):
         pass
 
-    def _setup(self):
+    def _get_obs(self):
+        pass
 
-        ## initiate simulation
-        self.client.resetSimulation()
-        self.client.setTimeStep(self.dt)
-        self.client.setGravity(0,0,-9.8)
+    def _get_rew(self):
+        pass
 
-        ## setup ground
-        plane = p.createCollisionShape(shapeType = p.GEOM_PLANE)
-        self.ground  = p.createMultiBody(0, plane)
-        p.changeDynamics(ground, -1, lateralFriction=0.0) 
+        
+
+
 
         
 
--- mrsearch_IG_RL/envs/tmp.py
+++
@@ -1,7 +0,0 @@
-import os
-
-PATH_SCRIPT = os.path.dirname(os.path.realpath(__file__))
-PATH_ROOT   = os.path.dirname(PATH_SCRIPT)
-
-print(PATH_SCRIPT)
-print(PATH_ROOT)
