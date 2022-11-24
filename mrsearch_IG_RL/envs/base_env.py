6936c7e18b4bc56d864624261c02a249ae7a5507
KyleM73 <kyle.morgenstein@gmail.com>
2022-11-24 02:56:38 -0500

    working lidar

--- mrsearch_IG_RL/envs/base_env.py
+++ mrsearch_IG_RL/envs/base_env.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import time
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -9,6 +10,8 @@ from gym import spaces, Env
 from stable_baselines3.common.env_checker import check_env
 
 import pybullet as p
+from pybullet import getQuaternionFromEuler as E2Q
+from pybullet import getEulerFromQuaternion as Q2E
 import pybullet_utils.bullet_client as bc
 import pybullet_data
 
@@ -30,19 +33,34 @@ class base_env(Env):
             p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
         self.training = training
-        
+
         ## simulation params
         self.horizon = self.cfg["simulation"]["horizon"]
         self.dt = self.cfg["simulation"]["dt"]
         self.pad_l = self.cfg["simulation"]["pad_l"]
-
+        self.entropy_decay = self.cfg["simulation"]["entropy_decay"]
+        
         ## environment params
         self.resolution = self.cfg["environment"]["resolution"]
         self.h = int(self.cfg["environment"]["height"] / self.resolution)
         self.w = int(self.cfg["environment"]["width"] / self.resolution)
-        self.fname = self.cfg["environment"]["filename"]
-        self.map = torch.from_numpy(plt.imread(PATH_DIR+self.fname)[:,:,-1])
+        self.map_fname = self.cfg["environment"]["filename"]
+        self.map = torch.from_numpy(plt.imread(PATH_DIR+self.map_fname)[:,:,-1])
         self.map_urdf = self.cfg["environment"]["urdf"]
+        self.target_urdf = self.cfg["environment"]["target_urdf"]
+        
+        ## robot params
+        self.robot_urdf = self.cfg["robot"]["urdf"]
+        self.robot_width = self.cfg["robot"]["width"]
+        self.robot_depth = self.cfg["robot"]["depth"]
+        self.robot_height = self.cfg["robot"]["height"]
+
+        ## LiDAR params
+        self.scan_range = self.cfg["lidar"]["range"]
+        self.FOV = 2 * math.pi * self.cfg["lidar"]["FOV"] / 360 #deg2rad
+        self.scan_density = 2 * math.pi / math.atan(self.resolution/self.scan_range)/2 # resolution / max(height, width)
+        self.num_scans = int(self.FOV / (2 * math.pi) * self.scan_density)
+        self.scan_angle = self.FOV / self.num_scans #rad
 
         ## model I/O
         self.obs_space = spaces.Box(low=-1,high=1,shape=(self.h,self.w),dtype=np.float32)
@@ -67,31 +85,84 @@ class base_env(Env):
         self.walls = p.loadURDF(self.map_urdf,useFixedBase=True)
         p.changeDynamics(self.walls, -1, lateralFriction=1.0)
 
-        ## setup robots
+        ## setup robot
+        self.pose,self.ori = self._get_random_pose()
+        self.robot = p.loadURDF(self.robot_urdf,basePosition=self.pose,baseOrientation=E2Q([0,0,self.ori]))
+        
+        ## setup target
+        self.target_pose,_ = self._get_random_pose()
+        self.target_pose[2] = 0.5
+        self.target = p.loadURDF(self.target_urdf,basePosition=self.target_pose,useFixedBase=True)
+        
+        ## initialize objects
+        for _ in range(10):
+            self.client.stepSimulation()
 
         ## reset entropy
-        self.entropy = torch.where(self.map==1,1,-1)
+        self.entropy = torch.where(self.map==1,1,0)
 
         self._get_obs()
         self._get_rew()
 
-    def step(self):
+    def step(self,action):
+
+        self.action = action
+
         self.client.stepSimulation()
 
     def render(self):
-        pass
+        fig = plt.figure()
+        im = plt.imshow(self.entropy)
+        plt.show()
 
     def _get_obs(self):
-        pass
+
+        self.pose, oris = p.getBasePositionAndOrientation(self.robot)
+        self.ori = Q2E(oris)[2]
+        self.vel,self.avel = p.getBaseVelocity(self.robot)
+        
+        self._get_scans()
+
+        for hit in self.scans:
+            hr,hc = self._xy2rc(*hit[0])
+            sr,sc = self._xy2rc(*self.pose[:2])
+            if hit[1]:
+                self.entropy[hr,hc] = 1
+            free = self.bresenham((sr,sc),(hr,hc))
+            for f in free:
+                self.entropy[f[0],f[1]] = -1
+        self.entropy = torch.where(self.map==1,1,self.entropy)
 
     def _get_rew(self):
         pass
 
+    def _get_scans(self):
+        origins = [[self.pose[0],self.pose[1],0.5] for i in range(self.num_scans)]
+        endpts = [
+            [
+                self.scan_range * math.cos(i*self.scan_angle + self.ori - self.FOV/2) + self.pose[0],
+                self.scan_range * math.sin(i*self.scan_angle + self.ori - self.FOV/2) + self.pose[1],
+                0.5
+            ]
+                for i in range(self.num_scans)]
+        scan_data = p.rayTestBatch(origins,endpts,numThreads=0)
+        self.scans = []
+        for i in range(self.num_scans):
+            if scan_data[i][0] != -1: #check if hit detected
+                #dist = (sum([(scan_data[i][3][j]-self.pose[j])**2 for j in range(2)]))**0.5
+                #ang = i*self.scan_angle + self.ori - self.FOV/2
+                self.scans.append([scan_data[i][3][:2],True]) #only append hit location, distance, and angle
+            else:
+                self.scans.append([endpts[i][:2],False])
+
     def _get_random_pose(self):
         while True:
-            pose = torch.round(torch.rand((2,))*torch.tensor([self.h,self.w])).to(int)
+            pose = torch.round(torch.rand((2,))*torch.tensor([self.h,self.w])).to(int).tolist()
             if self._is_valid(*pose):
-                return pose
+                pose = self._rc2xy(*pose)
+                pose.append(self.robot_height/2+1e-4)
+                ori = 2*math.pi*torch.rand((1)).item()-math.pi
+                return [pose,ori]
 
     def _is_valid(self,r,c,convert=False):
         if convert:
@@ -104,26 +175,72 @@ class base_env(Env):
         return True
 
     def _xy2rc(self,x,y):
-        r = max(min(-int(y/self.resolution) + int(self.h/2) + 1,self.h),0)
-        c = max(min(int(x/self.resolution) + int(self.w/2) + 1,self.w),0)
-        return r,c 
+        r = int(-y / self.resolution + (self.h + 1) / 2)
+        r = max(min(r,self.h),0)
+        c = int(x / self.resolution + (self.w + 1) / 2)
+        c = max(min(c,self.w),0)
+        return [r,c] 
 
     def _rc2xy(self,r,c):
-        x = self.resolution*(c - int(self.w/self.resolution)/2+1)
-        y = self.resolution*(-r + int(self.h/self.resolution)/2+1)
-        return x,y
+        x = self.resolution * (c - (self.w + 1) / 2 )
+        y = self.resolution * (-r + (self.h + 1) / 2)
+        return [x,y]
+
+    def bresenham(self,start,end):
+        """
+        Adapted from PythonRobotics:
+        https://github.com/AtsushiSakai/PythonRobotics/blob/master/Mapping/lidar_to_grid_map/lidar_to_grid_map.py
+
+        Implementation of Bresenham's line drawing algorithm
+        See en.wikipedia.org/wiki/Bresenham's_line_algorithm
+        Bresenham's Line Algorithm
+        Produces a list from start and end (original from roguebasin.com)
+        >>> points1 = bresenham((4, 4), (6, 10))
+        >>> print(points1)
+        [[4,4], [4,5], [5,6], [5,7], [5,8], [6,9], [6,10]]
+        """
+        # setup initial conditions
+        x1, y1 = start
+        x2, y2 = end
+        dx = x2 - x1
+        dy = y2 - y1
+        is_steep = abs(dy) > abs(dx)  # determine how steep the line is
+        if is_steep:  # rotate line
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
+        # swap start and end points if necessary and store swap state
+        swapped = False
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+            swapped = True
+        dx = x2 - x1  # recalculate differentials
+        dy = y2 - y1  # recalculate differentials
+        error = int(dx / 2.0)  # calculate error
+        y_step = 1 if y1 < y2 else -1
+        # iterate over bounding box generating points between start and end
+        y = y1
+        points = []
+        for x in range(x1, x2 + 1):
+            coord = [y, x] if is_steep else (x, y)
+            points.append(coord)
+            error -= abs(dy)
+            if error < 0:
+                y += y_step
+                error += dx
+        if swapped:  # reverse the list if the coordinates were swapped
+            points.reverse()
+        return points[1:-1] #do not include endpoints
 
 if __name__ == "__main__":
 
     env = base_env(False,PATH_DIR+"/cfg/base_env.yaml")
     env.reset()
+    env._get_scans()
 
-    print(env._get_random_pose())
-    assert False
-
-    for _ in range(100000):
+    for _ in range(1000000):
         env.step()
-        time.sleep(0.1)
+        time.sleep(0.01)
 
 
         
